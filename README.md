5977f56cea5f89167ed7bad731207257401e4cab
DanteMezcal <322037461@ciencias.unam.mx>
2026-05-19 18:26:54 -0600

    Primeras 4 funciones de la practica

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ Si no les llegan a funcionar, es posible que el problema es que tengan una versi
 
 En esta sección deben eliminar esta línea de texto, borrar la leyenda "Integrante n" y escribir su nombre empezando por apellidos y su número de cuenta.
 
-+ Integrante 1
-    - No. de Cuenta: 
++ Carriche Arriaga Dante Raziel
+    - No. de Cuenta: 32207461
 + Integrante 2
     - No. de Cuenta: 
 + Integrante 3
--- src/Practica05.hs
+++ src/Practica05.hs
@@ -4,24 +4,42 @@ import Terminos
 
 --Aplicar una sustitucion a un termino
 apsubT :: Term -> Subst -> Term
-apsubT = undefined 
-
+apsubT a [] = a
+apsubT (Var x) ((y, t):ys) =
+  if x == y
+  then t
+  else apsubT (Var x) ys
+apsubT (Fun f args) subst = Fun f (aplicarLista args subst)
+  
 --Funcion auxiliar para aplicar la sustitucion a una lista de terminos
 aplicarLista :: [Term] -> Subst -> [Term]
-aplicarLista = undefined 
+aplicarLista [] _ = []
+aplicarLista l [] = l
+aplicarLista (x:xs) (y:ys) = (apsubT x (y:ys)):(aplicarLista xs (y:ys))
 
 --Funcion que elimina los pares que son de la forma x=x
 simpSus :: Subst -> Subst
-simpSus = undefined
+simpSus [] = []
+simpSus ((x, Var n):xs) =
+  if x == n
+  then simpSus xs
+  else ((x, Var n):(simpSus xs))
+simpSus (f:xs) = f:(simpSus xs)
 
 --Funcion que calcula la composicion de dos sustituciones
 compSus :: Subst -> Subst -> Subst
-compSus = undefined
+compSus s1 s2 = compSusAux(parte1 ++ s2)
+  where parte1 = [(x, apsubT t s2) | (x,t) <- s1]
 
+--Funcion auxiliar para compSus
+compSusAux :: Subst -> Subst
+compSusAux [] = []
+compSusAux ((x,t):xs) =
+  (x,t):(compSusAux [(y,s) | (y,s) <- xs, y /= x])
+  
 --Funcion que devuelve un umg de dos terminos, si es que lo hay
 unifica :: Term -> Term -> [Subst]
 unifica = undefined
-
 
 --Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
 unificaListas :: [Term] -> [Term] -> [Subst]
