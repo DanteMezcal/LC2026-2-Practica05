e72e0d17a8fa9d66c0857bbbccd718d8f2f10d9c
Rafael <rspastrana@ciencias.unam.mx>
2026-05-22 18:54:58 -0600

    Agregando unifica y unificaListas

--- src/Practica05.hs
+++ src/Practica05.hs
@@ -39,11 +39,34 @@ compSusAux ((x,t):xs) =
   
 --Funcion que devuelve un umg de dos terminos, si es que lo hay
 unifica :: Term -> Term -> [Subst]
-unifica = undefined
+unifica (Var v1) t2
+  | Var v1 == t2 = [[]]
+  | v1 `elem` vars t2 = []
+  | otherwise = [[(v1, t2)]]
+unifica t1 (Var v2) = unifica (Var v2) t1
+unifica (Fun f1 args1) (Fun f2 args2)
+  | f1 == f2 && length args1 == length args2 = unificaListas args1 args2
+  | otherwise = []
+unifica _ _ = []
+
+--Funcion auxiliar para unifica, devuelve las variables de un termino
+vars :: Term -> [Nombre]
+vars (Var v) = [v]
+vars (Fun _ args) = concatMap vars args
 
 --Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
 unificaListas :: [Term] -> [Term] -> [Subst]
-unificaListas = undefined
+unificaListas [] [] = [[]]
+unificaListas (t1:ts1) (t2:ts2) = do
+  s1 <- unifica t1 t2
+  s2 <- unificaListas (apsubTList ts1 s1) (apsubTList ts2 s1)
+  return (compSus s2 s1)
+unificaListas _ _ = []
+
+--Funcion auxiliar para unificaListas, devuelve una lista de terminos aplicadas a una sustitucion
+apsubTList :: [Term] -> Subst -> [Term]
+apsubTList [] _ = []
+apsubTList (t:ts) s = apsubT t s : apsubTList ts s
 
 --Funcion que devuelve un umg de una lista de termino, si es que lo hay
 unificaConj :: [Term] -> [Subst]
