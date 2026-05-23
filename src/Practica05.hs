9a5407f3f18711d58beb6d88fcfd6c5a8d26db76
Mafer <ayala_montiel_marifer@ciencias.unam.mx>
2026-05-22 20:44:07 -0600

    unificaConj

--- src/Practica05.hs
+++ src/Practica05.hs
@@ -70,5 +70,10 @@ apsubTList (t:ts) s = apsubT t s : apsubTList ts s
 
 --Funcion que devuelve un umg de una lista de termino, si es que lo hay
 unificaConj :: [Term] -> [Subst]
-unificaConj = undefined
+unificaConj [] = [[]]
+unificaConj [_] = [[]]
 
+unificaConj (t1:t2:ts) = do
+  s1 <- unifica t1 t2
+  s2 <- unificaConj (aplicarLista (t2:ts) s1)
+  return (compSus s1 s2)
